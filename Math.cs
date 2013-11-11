7377c1f47794ff83bcef933ead29da3701e9df16
tungul <lugnut1206@gmail.com>
2013-11-11 08:59:25 -0800

    updated math.cs to latest version

--- Math.cs
+++ Math.cs
@@ -7,7 +7,7 @@
 // ------------------------------------------- //
 
 //Addition
-function Add(%num1, %num2)
+function Math_Add(%num1, %num2)
 {
 	//Check if we can use torque addition
 	if(strLen(%num1 @ %num2) < 7)
@@ -28,7 +28,7 @@ function Add(%num1, %num2)
 }
 
 //Subtraction
-function Subtract(%num1, %num2)
+function Math_Subtract(%num1, %num2)
 {
 	//Check if we can use torque subtraction
 	if(strLen(%num1 @ %num2) < 7)
@@ -49,18 +49,35 @@ function Subtract(%num1, %num2)
 }
 
 //Multiplication
-function Multiply(%num1,%num2)
+function Math_Multiply(%num1,%num2)
 {
 	//Check if we can use torque multiplication
-	if(strLen(%num1 @ %num2) < 7)
+	if((%a=strLen(%num1 @ %num2)) < 7)
 		return %num1 * %num2;
 	
 	//Here's a symbol you don't see everyday, the caret, AKA the XOR operator.
-	//This check will return true
 	if(%num1 < 0 ^ %num2 < 0)
-		return "-" @ strMul(%num1, %num2);
+		%ans = "-";
+	if(%Num1 < 0)
+		%Num1 = switchS(%Num1);
+	if(%Num2 < 0)
+		%Num2 = switchS(%Num2);
 	
-	return strMul(%num1,%num2);
+	//Check if it's better to use classic or karatsuba
+	if(%a - strLen(%num2) < 15 && %a - strLen(%num1) < 15)
+		return %ans @ strMul(%num1, %num2);
+	
+	return %ans @ Karat(%num1, %num2);
+}
+
+function Math_Pow(%num1, %num2)
+{
+	if(%num2 < 0)
+		return 0;
+	if(%num2 == 0 || %num1 == 1)
+		return 1;
+	
+	return expon(%num1, %num2);
 }
 
 //This function switches the sign on a number.
@@ -71,7 +88,25 @@ function switchS(%a)
 	return "-" @ %a;
 }
 
-//This function is the home of the multiplication algorithm.
+//decimal shift right
+function shiftRight(%a, %b)
+{
+	return (%c = strLen(%a)) > %b ? getSubStr(%a, 0, %c-%b) : "0"; 
+}
+
+//decimal shift left
+function shiftLeft(%f,%a)
+{
+	if(%f$="0")return%f;
+	%b = ~~(%a/32);
+	
+	%d="00000000000000000000000000000000";
+	for(%b = %b; %b > 0; %b--)
+		%c = %c @ %d;
+	
+	return (%e=%a%32) ? %f @ %c @ getSubStr(%d, 0, %e) : %f @ %c;
+}
+
 function strMul(%Num1,%Num2)
 {
 	if(%Num1 < 0)
@@ -82,16 +117,19 @@ function strMul(%Num1,%Num2)
 	%Len1 = strlen(%Num1);
 	%Len2 = strlen(%Num2);
 	
-	//This part on used to be a LOT more complicated and unnessescary.
 	for(%a = %Len2-1; %a >= 0; %a--)
 		for(%b = %Len1-1; %b >= 0; %b--)
 			%Products[%a+%b] += getSubStr(%Num2,%a,1)*getSubStr(%Num1,%b,1);
 	
-	%Column = %Len1 + %Len2 + strlen(%Products0) - 2;
+	%MaxColumn = %Len1 + %Len2 + strlen(%Products0) - 2;
+	%MaxUse = %MaxColumn - %Len1 - %Len2 + 2;
 	
 	for(%a = %Len2 + %Len1 - 2; %a >= 0; %a--)
+	{
+		%x = strLen(%Products[%a]);
 		for(%b = strlen(%Products[%a]) - 1; %b >= 0; %b--)
-			%Digits[%MaxColumn + %a - strLen(%Products[%a]) - %Len2 - %Len1 + %b + 2] += getSubStr(%Products[%a], %b, 1);
+			%Digits[%MaxUse + %a - %x + %b] += getSubStr(%Products[%a], %b, 1);
+	}
 	
 	for(%a=%MaxColumn-1;%a>=0;%a--)
 	{
@@ -103,11 +141,66 @@ function strMul(%Num1,%Num2)
 		}
 		else
 			%Carry = 0;
-		
 		%Result = %Temp @ %Result;
 	}
 	
 	return %Result;
+}
+
+//Karatsuba multiplication algorithm
+//This is faster than the classic multiplication for most numbers greater than 40 digits long.
+function Karat(%num1,%num2)
+{
+	%len1 = strLen(%num1); %len2 = strLen(%num2);
+	if(%len1 + %len2 <= 40 || %len1 < 5 || %len2 < 5)
+		return strMul(%num1, %num2);
+	%m = mCeil(getMax(%len1, %len2) / 2);
+	%y=%len1-%m;
+	%z=%len2-%m;
+	if(%num1 $= "0" || %num2 $= "0")
+		return "0";
+	if(%len1 <= %m)
+	{
+		%x0 = %num1;
+		%x1 = "0";
+		%y0 = getSubStr(%num2, %z, %len2);
+		%a = %len2 % %m;
+		if(%a == 0)
+			%y1 = getSubStr(%num2, 0, %m);
+		else
+			%y1 = getSubStr(%num2, 0, %a);
+	}
+	else if(%len2 <= %m)
+	{
+		%y0 = %num2;
+		%y1 = "0";
+		%x0 = getSubStr(%num1, %y, %len1);
+		%a = %len1 % %m;
+		if(%a == 0)
+			%x1 = getSubStr(%num1, 0, %m);
+		else
+			%x1 = getSubStr(%num1, 0, %a);
+	}
+	else
+	{
+		%x0 = getSubStr(%num1, %y, %len1);
+		%a = %len1 % %m;
+		if(%a == 0)
+			%x1 = getSubStr(%num1, 0, %m);
+		else
+			%x1 = getSubStr(%num1, 0, %a);
+		%y0 = getSubStr(%num2, %z, %len2);
+		%a = %len2 % %m;
+		if(%a == 0)
+			%y1 = getSubStr(%num2, 0, %m);
+		else
+			%y1 = getSubStr(%num2, 0, %a);
+	}
+	%z0 = Karat(%x0, %y0);
+	%z2 = Karat(%x1, %y1);
+	%z1 = stringSub(strMul(intadd(%x1, %x0), intadd(%y1, %y0)), intAdd(%z0, %z2));
+	%a = shiftLeft("", %m);
+	return strReplace(lTrim(strReplace(intAdd(intAdd(%z2 @ %a, %z1) @ %a, %z0), "0", " ")), " ", "0");
 }
 
 function intAdd(%num1,%num2)
@@ -119,8 +212,8 @@ function intAdd(%num1,%num2)
 	{
 		%d = %l1 - (%f-%a);
 		%e = %l2-(%f-%a);
-		%b = %d >=0? getSubStr(%Num1,%d,1) : "0";
-		%c = %e>=0 ? getSubStr(%Num2,%e,1) : "0";
+		%b = %d >=0 ? getSubStr(%Num1,%d,1) : "0";
+		%c = %e >=0 ? getSubStr(%Num2,%e,1) : "0";
 		%res = %b+%c+%Carry;
 		if(%res > 9 && %a != 0)
 		{
@@ -162,10 +255,7 @@ function stringSub(%num1, %num2)
 	if(%num1 < %num2)
 		return "-" @ stringSub(%num2, %num1);
 	
-	//I know, I know. I can hear you screaming through your monitor
-	//"BUT IPQ! TORQUE OPERATORS DON'T WORK FOR HUGE NUMBERS!"
-	//But you're wrong. They do. do echo(2309862398762935723 == 2309862398762935723); in your console and find out for yourself.
-	if(%num1 == %num2)
+	if(!strCmp(%num1, %num2))
 		return "0";
 	
 	%Length = strLen(%num1);
@@ -201,17 +291,30 @@ function stringSub(%num1, %num2)
 //This function equalises the length of two numbers by adding zeroes behind the shorter one.
 function equ0s(%num1, %num2)
 {
-	if(strLen(%num1) < strLen(%num2))
-	{
-		%b = strLen(%num2) - strLen(%num1);
-		for(%a=0;%a<%b;%a++)
-			%num1 = "0" @ %num1;
-	}
-	else if(strLen(%num1) > strLen(%num2))
-	{
-		%b = strLen(%num1) - strLen(%num2);
-		for(%a=0;%a<%b;%a++)
-			%num2 = "0" @ %num2;
-	}
+	%x = strLen(%num1); %y = strLen(%num2);
+	if(%x < %y)
+		%num1 = shiftLeft("", %y - %x) @ %num1;
+	else if(%x > %y)
+		%num2 = shiftLeft("", %x - %y) @ %num2;
+	
 	return %num1 SPC %num2;
+}
+
+
+function expon(%a, %b, %d)
+{
+	if(%b == 0)
+		return 1;
+	else if(%b < 0)
+		return expon(1/%a, -1 * %b, %d++);
+	else if(%b % 2 == 1)
+	{
+		%c = expon(%a, (%b - 1) / 2, %d++);
+		return Math_Multiply(%a, Math_Multiply(%c, %c));
+	}
+	else if(%b % 2 == 0)
+	{
+		%c = expon(%a, %b / 2, %d++);
+		return Math_Multiply(%c, %c);
+	}
 }
