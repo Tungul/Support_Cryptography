432b6ef63ebede812d90d43d4d2601186fe40e11
tungul <lugnut1206@gmail.com>
2013-11-13 23:53:13 -0800

    added working primality check and the fast exponential mod algorithm thing. neither uses APA, but that can be remidied later...

--- diffiepoc.cs
+++ diffiepoc.cs
@@ -22,10 +22,50 @@ $secret = mod(math_pow($bobPublic, $aliceSecret), $pubPrime);
 
 echo("Secret Num:" SPC $secret);
 
-//function isPrime(%num)
-//{
-	//if num % 2 == 0, false
-	//if num + 1 % 6 == 0 or num - 1 % 6 == 0, continue. otherwise false.
-	//loop through all odd numbers less than the square root of num, if divisible by any, false
-	//return true
-//}
+function isPrime(%num) // this is the basic function, it needs to be implemented utilizing APA (Abritrary Precision Arithmetic).
+{
+	if(%num % 2 == 0)
+		return false;
+
+	if(num + 1 % 6 == 0 || num - 1 % 6 == 0)
+	{
+		continue;
+	}
+	else
+	{
+		return false; // holy shit this is so fucking inefficient and probably broken. i totally screwed something up here.
+	}
+
+	%squareRoot = mSqrt(%num);
+
+	for(%i = 3; %i < %squareRoot; %i += 2) // this can be sped up if we generate a set of primes using the sieve of eratsones, or just flat out include a list of primes from like, one to a million.
+	{
+		if(%num % %i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+function Math_fastExpMod(%x, %e, %m) // this is the basic function, it needs to be implemented utilizing APA (Abritrary Precision Arithmetic).
+{
+	%y = 1;
+
+	%z = x;
+
+	while(%e > 0)
+	{
+		if(%e % 2 != 0)
+		{
+			%y = (%y * %z) % %m;
+		}
+
+		%z = (%z * %z) % %m;
+
+		%e = mFloor(%e / 2);
+	}
+
+	return %y;
+}
