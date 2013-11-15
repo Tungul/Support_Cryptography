009315fbca891c01134c80f81819781cff38a849
tungul <lugnut1206@gmail.com>
2013-11-15 14:08:32 -0800

    added big list of prime numbers, moved a function

--- diffiepoc.cs
+++ diffiepoc.cs
@@ -1,4 +1,5 @@
 exec("./lib/Math.cs");
+exec("./src/prime.cs")
 
 function mod(%a,%b)
 {
@@ -21,33 +22,6 @@ echo("Bob Public:" SPC $bobPublic);
 $secret = mod(math_pow($bobPublic, $aliceSecret), $pubPrime);
 
 echo("Secret Num:" SPC $secret);
-
-function isPrime(%num) // this is the basic function, it needs to be implemented utilizing APA (Abritrary Precision Arithmetic).
-{
-	if(%num % 2 == 0)
-		return false;
-
-	if(num + 1 % 6 == 0 || num - 1 % 6 == 0)
-	{
-		continue;
-	}
-	else
-	{
-		return false; // holy shit this is so fucking inefficient and probably broken. i totally screwed something up here.
-	}
-
-	%squareRoot = mSqrt(%num);
-
-	for(%i = 3; %i < %squareRoot; %i += 2) // this can be sped up if we generate a set of primes using the sieve of eratsones, or just flat out include a list of primes from like, one to a million.
-	{
-		if(%num % %i == 0)
-		{
-			return false;
-		}
-	}
-
-	return true;
-}
 
 function Math_fastExpMod(%x, %e, %m) // this is the basic function, it needs to be implemented utilizing APA (Abritrary Precision Arithmetic).
 {
---
+++ src/prime.cs
@@ -0,0 +1,22 @@
+function isPrime(%num) // this is the basic function, it needs to be implemented utilizing APA (Abritrary Precision Arithmetic).
+{
+	if(%num % 2 == 0)
+		return false;
+
+	if(num + 1 % 6 == 0 || num - 1 % 6 == 0)
+		continue;
+	else
+		return false; // holy shit this is so fucking inefficient and probably broken. i totally screwed something up here.
+
+	%squareRoot = mSqrt(%num);
+
+	for(%i = 3; %i < %squareRoot; %i += 2) // this can be sped up if we generate a set of primes using the sieve of eratsones, or just flat out include a list of primes from like, one to a million.
+	{
+		if(%num % %i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
