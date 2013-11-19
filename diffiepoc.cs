6eec9a4571ebae2f6d705722a5d25b3fff762c48
tungul <lugnut1206@gmail.com>
2013-11-19 00:15:55 -0800

    !!READ THIS GREEK AND IP!! made presumably working untested diffie hellman key exchange and OTP processing.
    just to get the ball rolling while i figure out ECC
    
    i commented a lot of the lines, can someone answer, handle, or otherwise do the things i mentioned?

--- diffiepoc.cs
+++ diffiepoc.cs
@@ -1,12 +1,8 @@
 exec("./lib/Math.cs");
 exec("./src/prime.cs");
+exec("./lib/rand.cs")
 
-$pubPrime = 23;
-$pubBase = 5;
-$aliceSecret = 5;
-$bobSecret = 15;
-
-echo("Secret Num:" SPC $secret);function Math_fastExpMod(%x, %e, %m) // this is the basic function, it needs to be implemented utilizing APA (Abritrary Precision Arithmetic).
+function Math_fastExpMod(%x, %e, %m) // should move this to ./lib/math.cs
 {
 	%y = "1";
 
@@ -27,12 +23,89 @@ echo("Secret Num:" SPC $secret);function Math_fastExpMod(%x, %e, %m) // this is 
 	return %y;
 }
 
-$alicePublic = Math_fastExpMod($pubBase, $aliceSecret, $pubPrime);
+new ScriptObject(DiffieHellman);
 
-echo("Alice Public:" SPC $alicePublic);
+function DiffieHellman::generateDiffie(%this, %prime, %base)
+{
+	// need a better random number function
+	%context = createRandContext();
+	// like i'm not even kidding, this thing will literally not work worth shit (no offense port)
+	if(%prime !$= "" || %base !$= "")
+	{
+		%this.prime = rand(%context, 1111111, 999999, 0);
+		while(!Math_isPrime(%this.prime))
+		{
+			%this.prime = rand(%context, 1111111, 999999, 0); //I get the feeling this won't work right.
+		}
 
-$bobPublic = Math_fastExpMod($pubBase, $bobSecret, $pubPrime);
+		%this.base = rand(%context, 1111111, 999999, 0);
+		echo("Public prime: " @ %this.prime);
+		echo("Public base: " @ %this.base);
+	}
+	%this.Private = rand(%context, 222222, 999999, 0);
+	%this.PublicKey = Math_fastExpMod(%this.base, %this.Private, %this.prime);
+	echo("Public Key: " @ %this.PublicKey);
+	return %this.PublicKey;
+}
 
-echo("Bob Public:" SPC $bobPublic);
+function DiffieHellman::finalizeDiffieHandshake(%this, %bobspublic)
+{
+	%this.Secret = Math_fastExpMod(%bobspublic, %this.Private, %this.prime);
+	%this.randomContext = createRandContext(%this.secret); // needs a constant context for one time pad generation
 
-$secret = Math_fastExpMod($bobPublic, $aliceSecret, $pubPrime);
+	return %this.Secret;
+}
+
+function DiffieHellman::getOTPNums(%this, %num) // generates an arbitrary number of one time pad numbers
+{
+	for(%i = 0; %i < %num; %i++)
+	{
+		%this.OTPNums = %this.OTPNums @ rand(%this.randomContext, 1, 25, 0) @ "\t"; // 1 and 26?
+	}
+}
+
+function DiffieHellman::encrypt(%this, %str)
+{
+	for(%i = 0; %i < strLen(%str); %i++)
+	{
+		%char = getSubStr(%str, %i, 0); // or 1?
+		if(%char $= "")
+		{
+			%outputStr = %outputStr @ " "; // can we += here?
+			continue;
+		}
+		%char = %this.alphaToNum(%char); // convert it to a numeral somehow. NOTE: only alphabetical characters. if it isn't alphabetical, strip it or make our OTP work with a large charset
+		%out = %this.numToAlpha((%char + getField(%this.OTPNums, 0)) % 26); // remove nums after use, get the first number
+		%outputStr = %outputStr @ %out;
+	}
+
+	return %outputStr;
+}
+
+function DiffieHellman::decrypt(%this, %str)
+{
+	for(%i = 0; %i < strLen(%str); %i++)
+	{
+		%char = getSubStr(%str, %i, 0); // or 1?
+		if(%char $= "")
+		{
+			%outputStr = %outputStr @ " "; // can we += here?
+			continue;
+		}
+		%char = %this.alphaToNum(%char); // convert it to a numeral somehow. NOTE: only alphabetical characters. if it isn't alphabetical, strip it or make our OTP work with a large charset
+		// %out = %this.numToAlpha((%char + getField(%this.OTPNums, 0)) % 26); // remove nums after use, get the first number // DECRYPTION IS JUST REVERSE, YES? CHECK.
+		%outputStr = %outputStr @ %out;
+	}
+
+	return %outputStr;
+}
+
+function DiffieHellman::alphaToNum(%this, %char)
+{
+	
+}
+
+function DiffieHellman::numToAlpha(%this, %char)
+{
+
+}
--- src/prime.cs
+++ src/prime.cs
@@ -1,21 +1,21 @@
-function isPrime(%num) // this is the basic function, it needs to be implemented utilizing APA (Abritrary Precision Arithmetic).
+function Math_isPrime(%num) // this is the basic function, it needs to be implemented utilizing APA (Abritrary Precision Arithmetic).
 {
-	if(%num == 1 || %num == 2 || %num == 3) //Happy, Xalos?
+	if(%num $= "1" || %num $= "2" || %num $= "3") //Happy, Xalos?
 		return true;
 
-	if(%num % 2 == 0)
+	if(Math_Mod(%num, 2) $= "0")
 		return false;
 
-	if(num + 1 % 6 == 0 || num - 1 % 6 == 0)
+	if(Math_Mod(Math_Add(%num, 1), 6) $= "0" || Math_Mod(Math_Subtact(num, 1), 6) $= "0")
 		continue;
 	else
 		return false; // holy shit this is so fucking inefficient and probably broken. i totally screwed something up here.
 
-	%squareRoot = mSqrt(%num);
+	%squareRoot = Math_SquareRoot(%num); // oops we don't have one of these...
 
-	for(%i = 3; %i < %squareRoot; %i += 2) // this can be sped up if we generate a set of primes using the sieve of eratsones, or just flat out include a list of primes from like, one to a million.
+	for(%i = "3"; Math_LessThan(%i, %squareRoot); %i = Math_Add(%i, 2)) // this can be sped up if we generate a set of primes using the sieve of eratsones, or just flat out include a list of primes from like, one to a million.
 	{
-		if(%num % %i == 0)
+		if(Math_Mod(%num, %i) $= "0")
 		{
 			return false;
 		}
@@ -23,3 +23,5 @@ function isPrime(%num) // this is the basic function, it needs to be implemented
 
 	return true;
 }
+
+// TODO: "Math_SquareRoot()" and "Math_LessThan()" or equivalent
