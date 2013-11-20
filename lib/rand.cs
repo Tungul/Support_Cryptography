c58779f005a205190d1177378ac7eae66378af2f
tungul <lugnut1206@gmail.com>
2013-11-19 21:20:42 -0800

    made rand.cs ... work with arbitrary precision?

--- lib/rand.cs
+++ lib/rand.cs
@@ -1,3 +1,5 @@
+// Modified by Lugnut to allow for arbitrary precision using Ipquarx's arbitrary precision arithmetic Math.cs.
+
 function createRandContext(%seed) {
 	if (%seed $= "") {
 		%seed = getRealTime();
@@ -10,28 +12,30 @@ function createRandContext(%seed) {
 }
 
 function rand(%id, %min, %max, %precision) {
-	%a = 7 * 47 + 1;
-	%c = 100;
-	%m = 48 * 48 - 1;
+	%a = Math_Add(Math_Multiply("7", "47"), "1");
+	%c = "100";
+	%m = Math_Subtract(Math_Multiply("48", "48"), "1");
 
 	%result = ($_RNGState[%id] = LCG($_RNGState[%id], %a, %c, %m)) / %m;
 
 	if (%max $= "") {
 		if (%min !$= "") {
-			%result = %result * %min;
+			%result = Math_Multiply(%result, %min);
 		}
 	}
 	else {
-		%result = %min + %result * (%max - %min);
+		%result = Math_Add(%min, Math_Multiply(%result, Math_Subtract(%max, %min)));
 	}
 
-	if (%precision !$= "") {
-		%result = mFloatLength(%result, %precision);
-	}
+	// if (%precision !$= "") { // removed and replaced to limit only to integers
+	// 	%result = mFloatLength(%result, %precision); 
+	// }
+
+	// does it need a replacement?
 
 	return %result;
 }
 
 function getRandSeed(%id) { return $_RNGSeed[%id]; }
 function setRandSeed(%id, %seed) { $_RNGState[%id] = $_RNGSeed[%id] = %seed; }
-function LCG(%state, %a, %c, %m) { return (%a * %state + %c) % %m; }
+function LCG(%state, %a, %c, %m) { return Math_Mod(Math_Add(Math_Multiply(%a, %state), %c), %m); }
